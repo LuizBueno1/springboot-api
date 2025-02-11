a197aaae329505da961bd99cf7aaa600c7ab0142
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-02-11 20:31:08 -0300

    Add Integration Test

--- src/main/java/com/example/springboot_startspringio/requests/AnimePostRequestBody.java
+++ src/main/java/com/example/springboot_startspringio/requests/AnimePostRequestBody.java
@@ -1,11 +1,15 @@
 package com.example.springboot_startspringio.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotBlank(message = "The anime name cannot be empty")
     private String name;
